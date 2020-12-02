f1c90937142d25866cd6924d069465ace1a58732
j-blalock <justin.m.blalock@gmail.com>
2020-12-01 18:22:00 -0600

    assoc routes in server.js

--- server.js
+++ server.js
@@ -2,22 +2,23 @@ const express = require("express");
 require("dotenv").config(); // for loading environment variables
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const passport = require("passport");
+const path = require("path");
+const users = require("./routes/api/users");
 
 const app = express();
 
-// Body Parser - puts request data on req.body
+// -------------------------- MIDDLEWARE
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const MONGO_URI = process.env.MONGO_URI;
-
-// -------------------------- MIDDLEWARE
-const passport = require("passport");
-
 app.use(passport.initialize());
 require("./middleware/passport")(passport);
+app.use("/api/users", users);
+app.use("/api/posts/", require("./routes/api/posts"));
 
 // -------------------------- DB CONFIG
+const MONGO_URI = process.env.MONGO_URI;
 mongoose
   .connect(MONGO_URI, {
     useNewUrlParser: true,
@@ -25,10 +26,17 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false,
   })
-  .then(() => console.log("Mongo Connection successful"))
+  .then(() => console.log("MongoDB connection successful"))
   .catch((err) => console.log("err"));
 
 mongoose.Promise = global.Promise;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // -------------------------- ROUTES
 app.use("/api/users/", require("./routes/api/users"));
